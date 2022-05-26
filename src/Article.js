dcb8d420002cdc3882ddaa4192097ead4962fd76
Isaiah <ijandicos90@gmail.com>
2022-05-26 05:49:21 -0400

    Completed Walkthrough

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function Article() {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.
+      I also completed walkthrough of React hooks basics
+    </div>
   );
 }
 
